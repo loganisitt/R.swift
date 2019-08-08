26172a097d5d33c625b8b64eda9bef47d7760aa6
Logan Isitt <logan.isitt@gmail.com>
2019-08-08 16:28:41 -0400

    use bclymer RswiftCore

--- Sources/RswiftCore/RswiftCore.swift
+++ Sources/RswiftCore/RswiftCore.swift
@@ -11,13 +11,8 @@ import Foundation
 import XcodeEdit
 
 public struct RswiftCore {
-    private let callInformation: CallInformation
 
-    public init(_ callInformation: CallInformation) {
-        self.callInformation = callInformation
-    }
-
-    public func run() throws {
+    static public func run(_ callInformation: CallInformation) throws {
         do {
             let xcodeproj = try Xcodeproj(url: callInformation.xcodeprojURL)
             let ignoreFile = (try? IgnoreFile(ignoreFileURL: callInformation.rswiftIgnoreURL)) ?? IgnoreFile()
@@ -29,8 +24,7 @@ public struct RswiftCore {
 
             let resources = Resources(resourceURLs: resourceURLs, fileManager: FileManager.default)
 
-            // Generate regular R file
-            let fileContents = generateRegularFileContents(resources: resources, generators: [
+            let generators: [StructGenerator] = [
                 ImageStructGenerator(assetFolders: resources.assetFolders, images: resources.images),
                 ColorStructGenerator(assetFolders: resources.assetFolders),
                 FontStructGenerator(fonts: resources.fonts),
@@ -40,18 +34,7 @@ public struct RswiftCore {
                 ReuseIdentifierStructGenerator(reusables: resources.reusables),
                 ResourceFileStructGenerator(resourceFiles: resources.resourceFiles),
                 StringsStructGenerator(localizableStrings: resources.localizableStrings),
-                AccessibilityIdentifierStructGenerator(nibs: resources.nibs, storyboards: resources.storyboards)
-                ])
-
-            writeIfChanged(contents: fileContents, toURL: callInformation.outputURL)
-
-            // Generate UITest R file
-            if let uiTestOutputURL = callInformation.uiTestOutputURL {
-                let uiTestFileContents = generateUITestFileContents(resources: resources, generators: [
-                    AccessibilityIdentifierStructGenerator(nibs: resources.nibs, storyboards: resources.storyboards)
-                    ])
-                writeIfChanged(contents: uiTestFileContents, toURL: uiTestOutputURL)
-            }
+            ]
 
             let aggregatedResult = AggregatedStructGenerator(subgenerators: generators)
                 .generatedStructs(at: callInformation.accessLevel, prefix: "")
@@ -113,6 +96,7 @@ public struct RswiftCore {
                     fail(error.localizedDescription)
                 }
             }
+
         } catch let error as ResourceParsingError {
             switch error {
             case let .parsingFailed(description):
@@ -125,56 +109,5 @@ public struct RswiftCore {
 
             exit(EXIT_FAILURE)
         }
-    }
-
-    private func generateRegularFileContents(resources: Resources, generators: [StructGenerator]) -> String {
-        let aggregatedResult = AggregatedStructGenerator(subgenerators: generators)
-            .generatedStructs(at: callInformation.accessLevel, prefix: "")
-
-        let (externalStructWithoutProperties, internalStruct) = ValidatedStructGenerator(validationSubject: aggregatedResult)
-            .generatedStructs(at: callInformation.accessLevel, prefix: "")
-
-        let externalStruct = externalStructWithoutProperties.addingInternalProperties(forBundleIdentifier: callInformation.bundleIdentifier)
-
-        let codeConvertibles: [SwiftCodeConverible?] = [
-            HeaderPrinter(),
-            ImportPrinter(
-                modules: callInformation.imports,
-                extractFrom: [externalStruct, internalStruct],
-                exclude: [Module.custom(name: callInformation.productModuleName)]
-            ),
-            externalStruct,
-            internalStruct
-        ]
-
-        return codeConvertibles
-            .compactMap { $0?.swiftCode }
-            .joined(separator: "\n\n")
-            + "\n" // Newline at end of file
-    }
-
-    private func generateUITestFileContents(resources: Resources, generators: [StructGenerator]) -> String {
-        let (externalStruct, _) =  AggregatedStructGenerator(subgenerators: generators)
-            .generatedStructs(at: callInformation.accessLevel, prefix: "")
-
-        let codeConvertibles: [SwiftCodeConverible?] = [
-            HeaderPrinter(),
-            externalStruct
-        ]
-
-        return codeConvertibles
-            .compactMap { $0?.swiftCode }
-            .joined(separator: "\n\n")
-            + "\n" // Newline at end of file
-    }
-}
-
-private func writeIfChanged(contents: String, toURL outputURL: URL) {
-    let currentFileContents = try? String(contentsOf: outputURL, encoding: .utf8)
-    guard currentFileContents != contents else { return }
-    do {
-        try contents.write(to: outputURL, atomically: true, encoding: .utf8)
-    } catch {
-        fail(error.localizedDescription)
     }
 }
